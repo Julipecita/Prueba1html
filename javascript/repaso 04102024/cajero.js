33405c1356da283b81cfa8cebfee70918d76ec4a
Julipecita <juliana.jep@gmail.comm>
2024-10-06 21:35:03 -0500

    repaso 041024

--- javascript/repaso 04102024/cajero.js
+++ javascript/repaso 04102024/cajero.js
@@ -8,14 +8,14 @@ class Cuenta{
     // Metosos setters y getters
 
     setCuenta(index, cuenta){
-        this._cuentas[index] = cuentas;
+        this._cuentas[index] = cuenta;
     }
 
     getCuenta(index){
         return this.cuentas[index];
     }
 
-    setSaldo(Index, cuenta){
+    setSaldo(index, saldo){
         this._saldos[index] = saldo;        
     }
 
@@ -25,17 +25,17 @@ class Cuenta{
     crearCuenta(){
         for (let i= 0; i < this._cuentas.length; i++) {         
         let cuenta = prompt(`Ingrese el Nuevo numero de Cuenta: ${i+1}`);
-        let saldo = parseFloat(prompt(`Ingrese el Saldo para la nueva cuanta:  ${cuenta} `));
+        let saldo = parseFloat(prompt(`Ingrese el Saldo para la nueva cuenta:  ${cuenta} `));
 
         this.setCuenta(i, cuenta);
         this.setSaldo(i, saldo);
         }
     }
-    consultarCuenta(){
+    consultarSaldo(){
         let cuenta = prompt(`Ingrese el numero de Cuenta a consultar saldo: `);
         let index = this._cuentas.indexOf(cuenta); // Busca el indice del valor como argumento
         if(index !== -1){
-            alert(`el saldo de la cuanta  ${cuenta} es de ${this.getSaldo(index)}`);
+            alert(`el saldo de la cuenta  ${cuenta} es de ${this.getSaldo(index)}`);
         }
         else{
         alert("Cuenta no existe")
@@ -43,17 +43,44 @@ class Cuenta{
 
     }
     consignarCuenta(){
+        let cuenta = prompt(`Ingrese el numero de Cuenta: `);
+        let index = this._cuentas.indexOf(cuenta); // Busca el indice del valor como argumento
+        if(index !== -1){
+            let nuevoSaldo = parseFloat(prompt("Ingrese Valor a consignar"));
+            this._saldos[index] = this._saldos[index]+nuevoSaldo
+            alert(`Nuevo Saldo es ${this.getSaldo(index)}`);
+        }
+        else{
+        alert("Cuenta no existe")
+        }
 
     }
 
     retiroCuenta(){
-
+        let cuenta = prompt(`Ingrese el numero de Cuenta: `);
+        let index = this._cuentas.indexOf(cuenta); // Busca el indice del valor como argumento
+        if(index !== -1){
+            let nuevoSaldo = parseFloat(prompt("Ingrese Valor a retirar"));
+            this._saldos[index] = this._saldos[index]-nuevoSaldo;
+            if(this._saldos[index] < 0){
+                alert("Saldo insuficiente, Debe pedir menos cantidad");
+                this._saldos[index] = this._saldos[index]+nuevoSaldo
+            }else{
+            alert(`Nuevo Saldo es ${this.getSaldo(index)}`);
+        }
+            }
+            else{
+        alert("Cuenta no existe")
 
     }
+        }
 
 }
 
+
+
 const cuenta = new Cuenta();
+
 
 //Menu interactivo con el usuario
 
@@ -61,7 +88,7 @@ let opcion;
 
 do{
 
-    opcion =parseInt(prompt(`***** MENU CAJERO BANCAYA*****
+    opcion = parseInt(prompt(`***** MENU CAJERO BANCAYA*****
         1. Crear Cuenta
         2. Consultar Saldos
         3. Consignacion
